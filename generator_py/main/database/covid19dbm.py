c477810e90e16cd38c32cc7f9fae14172be6e45b
osvaldoairon <osvaldo_cavalcanti_filho@hotmail.com>
2020-03-20 18:40:44 -0300

    creating_entity_mongodb

--- generator_py/main/database/covid19dbm.py
+++ generator_py/main/database/covid19dbm.py
@@ -1,3 +1,22 @@
-from pymongo import MongoClient
+import pymongo
+
+
+## Define o object database mongo db
+## Define o object operador do mongo db
+
+class DatabaseM(object):
+    def __init__(self, URL_MONGO_DB):
+        self.url = URL_MONGO_DB
+    
+    def get_instance(self):
+        if self.url != None:
+            self.client_instance = pymongo.MongoClient(self.url)
+            print(self.client_instance.test)
+
+            return self.client_instance
+        
+        return None
+    
+
 
 
