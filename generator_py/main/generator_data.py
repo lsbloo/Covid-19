3f6b3ea5d4a81a19064db5a82160fe9aabead5bd
osvaldoairon <osvaldo_cavalcanti_filho@hotmail.com>
2020-03-30 21:07:42 -0300

    menu

--- generator_py/main/generator_data.py
+++ generator_py/main/generator_data.py
@@ -183,159 +183,238 @@ def drop_metrics():
     operador_db = OperatorDatabase(db)
     return operador_db.drop_table_schemas_02()
 
-args = []
-for parameter in sys.argv[1:]:
-    args.append(parameter)
 
-if args[0] == 'collect':
-    print('collect size', len(get_all_metrics_collected()))
+def main():
+    args = []
+    for parameter in sys.argv[1:]:
+        args.append(parameter)
 
-    if args[1] == 'drop':
-        print("Drop Table Schema-2", drop_metrics())
-        
+    if args[0] == 'collect':
+        print('collect size', len(get_all_metrics_collected()))
 
-if args[0] == 'plot':
-    if args[1] == 'insert':
-        q = Generator.get_dataset_insert_operation(os.environ.get('HOME'),'metrics.csv')
-        mPandas = Pandas(os.environ.get('HOME'),'metrics.csv')
+        if args[1] == 'drop':
+            print("Drop Table Schema-2", drop_metrics())
+            
 
-        data_set= q[1:]
-        times_mb=[]
-        times_psql=[]
-        lines_mb =[]
-        lines_psql=[]
+    if args[0] == 'plot':
+        if args[1] == 'insert':
+            q = Generator.get_dataset_insert_operation(os.environ.get('HOME'),'metrics.csv')
+            mPandas = Pandas(os.environ.get('HOME'),'metrics.csv')
 
-        for i in data_set:
-            if i[0] == 'mongo':
-                times_mb.append(i[2])
-                lines_mb.append(i[4])
-            elif i[0] == 'psql':
-                times_psql.append(i[2])
-                lines_psql.append(i[4])
-        
-        quot = quot_insert(times_mb,times_psql,lines_mb,lines_psql)
-        df = mPandas.data_frame(quot)
-        
-        print(df)
+            data_set= q[1:]
+            times_mb=[]
+            times_psql=[]
+            lines_mb =[]
+            lines_psql=[]
 
-        time_and_mongo= [quot.get('times')[0],quot.get('lines')[0]]
-        time_and_psql = [quot.get('times')[1], quot.get('lines')[1]]
+            for i in data_set:
+                if i[0] == 'mongo':
+                    times_mb.append(i[2])
+                    lines_mb.append(i[4])
+                elif i[0] == 'psql':
+                    times_psql.append(i[2])
+                    lines_psql.append(i[4])
+            
+            quot = quot_insert(times_mb,times_psql,lines_mb,lines_psql)
+            df = mPandas.data_frame(quot)
+            
+            print(df)
 
-        time_mong_x = time_and_mongo[0]
-        line_mongo_y = time_and_mongo[1]
-        
-        time_psql_x = time_and_psql[0]
-        line_psql_y = time_and_psql[1]
+            time_and_mongo= [quot.get('times')[0],quot.get('lines')[0]]
+            time_and_psql = [quot.get('times')[1], quot.get('lines')[1]]
 
-        plt.bar( time_mong_x, line_mongo_y, label = 'MongoDB', color = 'r')
-        plt.bar( time_psql_x, line_psql_y , label = 'Postgres-SQL', color = 'b')
-        plt.legend()
-        plt.title('Desempenho de Inserção')
-        plt.savefig(os.environ.get('HOME')+"/metric_insert.pdf")
-        plt.show()
+            time_mong_x = time_and_mongo[0]
+            line_mongo_y = time_and_mongo[1]
+            
+            time_psql_x = time_and_psql[0]
+            line_psql_y = time_and_psql[1]
 
-    
+            plt.bar( time_mong_x, line_mongo_y, label = 'MongoDB', color = 'r')
+            plt.bar( time_psql_x, line_psql_y , label = 'Postgres-SQL', color = 'b')
+            plt.legend()
+            plt.title('Desempenho de Inserção')
+            plt.savefig(os.environ.get('HOME')+"/metric_insert.pdf")
+            plt.show()
 
-    elif args[1] == 'recovery':
-        q = Generator.get_dataset_recovery_operation(os.environ.get('HOME'),'metrics.csv')
-        mPandas = Pandas(os.environ.get('HOME'),'metrics.csv')
+        elif args[1] == 'recovery':
+            q = Generator.get_dataset_recovery_operation(os.environ.get('HOME'),'metrics.csv')
+            mPandas = Pandas(os.environ.get('HOME'),'metrics.csv')
 
-        data_set = q
-        times_mb=[]
-        times_psql=[]
-        lines_mb =[]
-        lines_psql=[]
+            data_set = q
+            times_mb=[]
+            times_psql=[]
+            lines_mb =[]
+            lines_psql=[]
 
-        for i in data_set:
-            if i[0] == 'mongo':
-                times_mb.append(i[2])
-                lines_mb.append(i[4])
-            elif i[0] == 'psql':
-                times_psql.append(i[2])
-                lines_psql.append(i[4])
-        
-        quot = quot_insert(times_mb,times_psql,lines_mb,lines_psql)
-        df = mPandas.data_frame(quot)
-        
-        print(df)
+            for i in data_set:
+                if i[0] == 'mongo':
+                    times_mb.append(i[2])
+                    lines_mb.append(i[4])
+                elif i[0] == 'psql':
+                    times_psql.append(i[2])
+                    lines_psql.append(i[4])
+            
+            quot = quot_insert(times_mb,times_psql,lines_mb,lines_psql)
+            df = mPandas.data_frame(quot)
+            
+            print(df)
 
-        time_and_mongo= [quot.get('times')[0],quot.get('lines')[0]]
-        time_and_psql = [quot.get('times')[1], quot.get('lines')[1]]
+            time_and_mongo= [quot.get('times')[0],quot.get('lines')[0]]
+            time_and_psql = [quot.get('times')[1], quot.get('lines')[1]]
 
-        time_mong_x = time_and_mongo[0]
-        line_mongo_y = time_and_mongo[1]
-        
-        time_psql_x = time_and_psql[0]
-        line_psql_y = time_and_psql[1]
+            time_mong_x = time_and_mongo[0]
+            line_mongo_y = time_and_mongo[1]
+            
+            time_psql_x = time_and_psql[0]
+            line_psql_y = time_and_psql[1]
 
-        plt.bar( time_mong_x, line_mongo_y, label = 'MongoDB', color = 'r')
-        plt.bar( time_psql_x, line_psql_y , label = 'Postgres-SQL', color = 'b')
-        plt.legend()
-        plt.title('Desempenho de Leitura')
-        plt.savefig(os.environ.get('HOME')+"/metric_recovery.pdf")
-        plt.show()
+            plt.bar( time_mong_x, line_mongo_y, label = 'MongoDB', color = 'r')
+            plt.bar( time_psql_x, line_psql_y , label = 'Postgres-SQL', color = 'b')
+            plt.legend()
+            plt.title('Desempenho de Leitura')
+            plt.savefig(os.environ.get('HOME')+"/metric_recovery.pdf")
+            plt.show()
+        elif args[1] == 'recovery-all':
+            q = Generator.get_dataset_recovery_all_operation(os.environ.get('HOME'),'metrics.csv')
+            mPandas = Pandas(os.environ.get('HOME'),'metrics.csv')
+            data_set = q
+            times_mb=[]
+            times_psql=[]
+            lines_mb =[]
+            lines_psql=[]
+            for i in data_set:
+                if i[0] == 'mongo':
+                    times_mb.append(i[2])
+                    lines_mb.append(i[4])
+                elif i[0] == 'psql':
+                    times_psql.append(i[2])
+                    lines_psql.append(i[4])
+            
+            quot = quot_insert(times_mb,times_psql,lines_mb,lines_psql)
+            df = mPandas.data_frame(quot)
+            
+            print(df)
+
+            time_and_mongo= [quot.get('times')[0],quot.get('lines')[0]]
+            time_and_psql = [quot.get('times')[1], quot.get('lines')[1]]
+
+            time_mong_x = time_and_mongo[0]
+            line_mongo_y = time_and_mongo[1]
+            
+            time_psql_x = time_and_psql[0]
+            line_psql_y = time_and_psql[1]
+
+            plt.bar( time_mong_x, line_mongo_y, label = 'MongoDB', color = 'r')
+            plt.bar( time_psql_x, line_psql_y , label = 'Postgres-SQL', color = 'b')
+            plt.legend()
+            plt.title('Desempenho de Leitura')
+            plt.savefig(os.environ.get('HOME')+"/metric_recovery_all.pdf")
+            plt.show()
 
 
-if args[0] == 'psql':
-    if args[1] == 'insert':
-        result_data_set = get_data_csv()
-        if args[2].isnumeric:
+
+    if args[0] == 'psql':
+        if args[1] == 'insert':
+            result_data_set = get_data_csv()
+            if args[2].isnumeric:
+                time_cal_psql_init = timeit.default_timer()
+                insert_data_psql(result_data_set,int(args[2]))
+                time_cal_psql_fim = timeit.default_timer()
+                metrics_time = time_insert_data(time_cal_psql_init,time_cal_psql_fim)
+                obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],args[2])
+                result = insert_metrics(obj_m)
+                print('Metric Insertd: ', result)
+
+
+    if args[0] == "psql":
+        if args[1] == 'recovery':
             time_cal_psql_init = timeit.default_timer()
-            insert_data_psql(result_data_set,int(args[2]))
+            re = recovery_data_psql(int(args[2]))
             time_cal_psql_fim = timeit.default_timer()
             metrics_time = time_insert_data(time_cal_psql_init,time_cal_psql_fim)
-            obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],args[2])
+            obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(re))
             result = insert_metrics(obj_m)
             print('Metric Insertd: ', result)
+        
+        if args[1] == 'drop':
+            re = drop_table_psql_schemas01()
+            print('Drop Table Schema-1: ', re )
 
-
-if args[0] == "psql":
-    if args[1] == 'recovery':
-        time_cal_psql_init = timeit.default_timer()
-        re = recovery_data_psql(int(args[2]))
-        time_cal_psql_fim = timeit.default_timer()
-        metrics_time = time_insert_data(time_cal_psql_init,time_cal_psql_fim)
-        obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(re))
-        result = insert_metrics(obj_m)
-        print('Metric Insertd: ', result)
     
-    if args[1] == 'drop':
-        re = drop_table_psql_schemas01()
-        print('Drop Table Schema-1: ', re )
 
- 
+    if args[0] == 'mongo':
+        if args[1] == 'insert':
+            result_data_set = get_data_csv()
+            if args[2].isnumeric:
+                time_cal_mongo_init = timeit.default_timer()
+                insert_data_mongo(result_data_set,int(args[2]))
+                time_cal_mongo_fim = timeit.default_timer()
+                metrics_time = time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
+                obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],args[2])
+                result = insert_metrics(obj_m)
 
-if args[0] == 'mongo':
-    if args[1] == 'insert':
-        result_data_set = get_data_csv()
-        if args[2].isnumeric:
+        elif args[1] == 'recovery-all-fast':
             time_cal_mongo_init = timeit.default_timer()
-            insert_data_mongo(result_data_set,int(args[2]))
+            q = recovery_data_mongo()
             time_cal_mongo_fim = timeit.default_timer()
+            time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
             metrics_time = time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
-            obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],args[2])
+            obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(q))
             result = insert_metrics(obj_m)
 
-    elif args[1] == 'recovery-all-fast':
-        time_cal_mongo_init = timeit.default_timer()
-        q = recovery_data_mongo()
-        time_cal_mongo_fim = timeit.default_timer()
-        time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
-        metrics_time = time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
-        obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(q))
-        result = insert_metrics(obj_m)
+        elif args[1] == 'recovery':
+            time_cal_mongo_init = timeit.default_timer()
+            q = recovery_data_mongo_one_one()
+            time_cal_mongo_fim = timeit.default_timer()
+            time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
+            metrics_time = time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
+            obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(q))
+            result = insert_metrics(obj_m)
 
-    elif args[1] == 'recovery':
-        time_cal_mongo_init = timeit.default_timer()
-        q = recovery_data_mongo_one_one()
-        time_cal_mongo_fim = timeit.default_timer()
-        time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
-        metrics_time = time_insert_data(time_cal_mongo_init,time_cal_mongo_fim)
-        obj_m = Metric(args[0],args[1],metrics_time[0],metrics_time[1],len(q))
-        result = insert_metrics(obj_m)
+        elif args[1] == 'drop':
+            print("Drop Collection: ", drop_collection_mong())
 
-    elif args[1] == 'drop':
-        print("Drop Collection: ", drop_collection_mong())
+def show_menu():
+    print("---------------------------------------------------------------------------")
+    print(" Operations: ")
+    print()
+    print("Insert Mongo DB or PSQL")
+    print()
+    print("python3 generate_data.py psql insert <number_lines>")
+    print("python3 generate_data.py psql mongo <number_lines>")
+    print()
+    print("Recovery")
+    print()
+    print("python3 generate_data.py mongo recovery")
+    print("python3 generate_data.py psql recovery <number_lines>")
+    print()
+    print("Drop")
+    print("python3 generate_data.py psql drop")
+    print("python3 generate_data.py mongo drop")
+    print()
+    print("Collect")
+    print("python3 generate_data.py collect")
+    print("python3 generate_data.py collect drop")
+    print()
+    print("Ploting")
+    print("python3 generate_data.py plot insert")
+    print("python3 generate_data.py plot recovery")
+    print("python3 generate_data.py plot recovery-all")
+    print()
+    print()
+    print("---------------------------------------------------------------------------")
+
+    print("Scripter: lsbloo")
+
+
+
+try:
+    show_menu()
+
+    main()
+    
+except Exception as e:
+    print('Error Input Param', e)
 
         
 
